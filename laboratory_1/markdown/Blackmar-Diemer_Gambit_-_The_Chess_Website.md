804f93d9d225b205ad042a960967840163bb5aa2
bogdan <bogdan.petraszczuk@gmail.com>
2024-03-14 22:00:30 +0100

    First version of simply dinamic web page.

---
+++ laboratory_1/markdown/Blackmar-Diemer_Gambit_-_The_Chess_Website.md
@@ -0,0 +1,4 @@
+# Blackmar-Diemer Gambit - The Chess Website
+The Blackmar-Diemer Gambit is one of the most ambitious attempts for white to gain a developmental advantage.<br>1. d4 d5
+2. e4 dxe4
+3. Nc3<br>From the second move white looks to give away his king’s pawn. Although many top GM’s find this to be unsound, many club level players have great success with it and if you are a very aggressive player I recommend playing it from time to time.<br>
---
+++ laboratory_1/server.py
@@ -0,0 +1,42 @@
+from flask import Flask, redirect, url_for, render_template
+import glob
+import markdown
+
+app = Flask(__name__)		# Setting up the application
+
+# Sets the route for each subpage.
+# Converts an markdown file to the html file.
+@app.route('/opening/<file_name>')
+def convert_to_html(file_name):
+    path = f"markdown/{file_name}"
+
+    # Opens file and saves it markdown version
+    # to html and returns it.
+    with open(path, "r") as f:
+        text = f.read()
+        html = markdown.markdown(text)
+
+    # Create a home link.
+    html += f'<p><a href="/"> Go to Main page </a></p>'
+
+    return html
+
+# Sets the route for the main page.
+@app.route('/')		
+def home():
+
+    # Takes all .md files from markdown folder
+    # glob returns pathes to those files.
+    all_md = glob.glob("markdown/*.md")
+
+    # render from template
+    for i in range(len(all_md)):
+        all_md[i] = all_md[i].replace("markdown/", "")
+        all_md[i] = all_md[i][:-len(".md")]
+
+    html = render_template('main_page.html', all_md=all_md)
+
+    
+    return html	
+
+app.run()
